0ac9f029b1aa4acd2a7e8b3333108bd4eee28ec2
Ivan Shimko <shimkoiw@gmail.com>
2023-03-08 02:33:34 +0300

    feat: change styles

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ window.onload = () => {
       {
         parentId: "buttonContainer",
         view: "button",
-        buttonTheme: "light",
+        buttonTheme: "dark",
         buttonSize: "xxl",
         buttonBorderRadius: 15,
       }
