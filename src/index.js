0d2c19c502b6bff8eb3b231c2369ecf69d4581f3
Ivan Shimko <shimkoiw@gmail.com>
2023-03-08 01:59:11 +0300

    feat: add welcome msg

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const authorize = ({
   const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
   const nameHtml = `<div class="name">${displayName}</div>`;
 
-  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+  document.getElementById("auth").innerHTML = `Привет, ${avatarHtml}${nameHtml}!`;
 };
 
 // Делаем запрос за инфой о пользователе.
